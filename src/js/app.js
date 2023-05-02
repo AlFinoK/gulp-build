244768d8c999e41cba8aee36d8189c0293372266
Nikita <nikitamalashkin73@gmail.com>
2023-05-02 15:24:01 +0600

    version 1.0

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import { testWebP } from './modules/functions.js'
+testWebP()
+
+// import Swiper from 'swiper/bundle'
+
+// new Swiper('.swiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: true,
+//   },
+// })
